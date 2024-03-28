58bb9d9972f1c92a03574e84f75e816c67791fe3
Onur Baglan <147552919+OnurBaglan@users.noreply.github.com>
2024-03-28 15:45:35 +0100

    api reading refactoring

---
+++ PortfolioCalculatorApp/Model/BusinessModel/API/ApiGetRequest.cs
@@ -0,0 +1,42 @@
+﻿
+
+namespace PortfolioCalculatorApp.Model.BusinessModel.API;
+
+internal class ApiGetRequest
+{
+    public ApiSources ApiSource { get; init; }
+
+    public string? StockSymbol { get; init; }
+
+    private readonly DateTime _date;
+
+    public string DateInFormat { get; init; }    
+
+    public string? Currency { get; init; }
+
+    public ApiGetRequest(ApiSources apiSource,
+        DateTime date, string? stockSymbol, string? currency)
+    {
+        ApiSource = apiSource;
+        _date = date;
+
+        DateInFormat = $"{_date.Year}-{_date.Month}-{_date.Day}";
+
+        switch (apiSource)
+        {
+            case ApiSources.MarketDataApp:
+                ArgumentNullException.ThrowIfNull(stockSymbol);
+                break;
+            case ApiSources.CurrencyBeacon:
+                ArgumentNullException.ThrowIfNull(currency);
+                break;
+
+        }
+
+        StockSymbol = stockSymbol;
+        Currency = currency;
+
+    }
+
+}
+
--- PortfolioCalculatorApp/Model/BusinessModel/API/ApiReader.cs
+++ PortfolioCalculatorApp/Model/BusinessModel/API/ApiReader.cs
@@ -8,81 +8,127 @@ namespace PortfolioCalculatorApp.Model.BusinessModel.API;
 
 public class ApiReader
 {
-    private readonly IMainAppFormView _view;
-    private readonly string[] _apiKeys;
-
-    public ApiReader(IMainAppFormView view)
+    private readonly Dictionary<int, string> _urls;
+    public ApiReader()
     {
-        _view = view;
+        _urls = new Dictionary<int, string>()
+{
+    {1,"https://api.marketdata.app/v1/stocks/candles/D/{0}/?from={1}&to={1}&token={2}" },
+    {2,"https://api.currencybeacon.com/v1/historical?base=USD&date={0}&symbols={1}&api_key={2}" },
+    {3,"https://api.metals.dev/v1/timeseries?start_date={0}&end_date={0}&api_key={1}" }
+
+};
     }
 
-    internal async Task<decimal> ConvertTo(string currency, decimal rawValue, DateTime purchaseDate)
+    public async Task<string> ReadRawData(ApiGetRequest request)
     {
-        var url = "https://api.currencybeacon.com/v1/historical?base=USD&date={0}&symbols={1}&api_key={2}";
-        var dateInFormat = $"{purchaseDate.Year}-{purchaseDate.Month}-{purchaseDate.Day}";
 
+        string date = request.DateInFormat;
+        string? stockSymbol = request.StockSymbol;
+        string? currency = request.Currency;
+        ApiSources source = request.ApiSource;
+
+        
         using HttpClient client = new HttpClient();
         {
-            var response = await client.GetAsync(string.Format(url, dateInFormat, currency, _view.ApiKey2));
-
-            response.EnsureSuccessStatusCode();
-
-            var jsonText = await response.Content.ReadAsStringAsync();
-
-            var result = ConvertJsonTextToCurrencyValue(jsonText, currency);
-
-            return result;
-
-
-        }
-    }
-
-    private decimal ConvertJsonTextToCurrencyValue(string jsonText, string currency)
-    {
-        JsonDocument jsonDoc = JsonDocument.Parse(jsonText);
-
-        JsonElement root = jsonDoc.RootElement;
-
-        JsonElement valueElement = root
-            .GetProperty("response")
-            .GetProperty("rates")
-            .GetProperty(currency);
-
-        decimal value = valueElement.GetDecimal();
-
-        jsonDoc.Dispose();
-
-        return value;
-    }
-
-    internal async Task<decimal> GetStockValue(string stockSymbol, DateTime purchaseDate)
-    {
-        var url = "https://api.marketdata.app/v1/stocks/candles/D/{0}/?from={1}&to={1}&token={2}";
-        var dateInFormat = $"{purchaseDate.Year}-{purchaseDate.Month}-{purchaseDate.Day}";
-
-        using HttpClient client = new HttpClient();
-        {
-            var response = await client.GetAsync(string.Format(url, "NVDA", dateInFormat, _view.ApiKey1));
-
-            response.EnsureSuccessStatusCode();
-
-            var jsonText = await response.Content.ReadAsStringAsync();
-
-            var result = ConvertJsonTextToStockValue(jsonText);
-
-            return result;
-
+            if (source == ApiSources.MarketDataApp)
+            {
+                var response = await client.GetAsync
+                    (string.Format(_urls[(int)source], stockSymbol, date, Environment.GetEnvironmentVariable("1")));
+            }
+            if (source == ApiSources.CurrencyBeacon)
+            {
+                var response = await client.GetAsync
+                    (string.Format(_urls[(int)source], stockSymbol, date, Environment.GetEnvironmentVariable("1")));
+            }
 
         }
+
+
+
+
     }
 
-    private decimal ConvertJsonTextToStockValue(string jsonText)
-    {
-        var stockCandle = JsonSerializer.Deserialize<StockCandle>(jsonText);
 
-        var result = (decimal)stockCandle.c.FirstOrDefault();
 
-        return result;
-    }
+
+
+
+    //private readonly IMainAppFormView _view;
+    //private readonly string[] _apiKeys;
+
+    //public ApiReader(IMainAppFormView view)
+    //{
+    //    _view = view;
+    //}
+
+    //internal async Task<decimal> ConvertTo(string currency, decimal rawValue, DateTime purchaseDate)
+    //{
+    //    var url = "https://api.currencybeacon.com/v1/historical?base=USD&date={0}&symbols={1}&api_key={2}";
+    //    var dateInFormat = $"{purchaseDate.Year}-{purchaseDate.Month}-{purchaseDate.Day}";
+
+    //    using HttpClient client = new HttpClient();
+    //    {
+    //        var response = await client.GetAsync(string.Format(url, dateInFormat, currency, _view.ApiKey2));
+
+    //        response.EnsureSuccessStatusCode();
+
+    //        var jsonText = await response.Content.ReadAsStringAsync();
+
+    //        var result = ConvertJsonTextToCurrencyValue(jsonText, currency);
+
+    //        return result;
+
+
+    //    }
+    //}
+
+    //private decimal ConvertJsonTextToCurrencyValue(string jsonText, string currency)
+    //{
+    //    JsonDocument jsonDoc = JsonDocument.Parse(jsonText);
+
+    //    JsonElement root = jsonDoc.RootElement;
+
+    //    JsonElement valueElement = root
+    //        .GetProperty("response")
+    //        .GetProperty("rates")
+    //        .GetProperty(currency);
+
+    //    decimal value = valueElement.GetDecimal();
+
+    //    jsonDoc.Dispose();
+
+    //    return value;
+    //}
+
+    //internal async Task<decimal> GetStockValue(string stockSymbol, DateTime purchaseDate)
+    //{
+    //    var url = "https://api.marketdata.app/v1/stocks/candles/D/{0}/?from={1}&to={1}&token={2}";
+    //    var dateInFormat = $"{purchaseDate.Year}-{purchaseDate.Month}-{purchaseDate.Day}";
+
+    //    using HttpClient client = new HttpClient();
+    //    {
+    //        var response = await client.GetAsync(string.Format(url, "NVDA", dateInFormat, _view.ApiKey1));
+
+    //        response.EnsureSuccessStatusCode();
+
+    //        var jsonText = await response.Content.ReadAsStringAsync();
+
+    //        var result = ConvertJsonTextToStockValue(jsonText);
+
+    //        return result;
+
+
+    //    }
+    //}
+
+    //private decimal ConvertJsonTextToStockValue(string jsonText)
+    //{
+    //    var stockCandle = JsonSerializer.Deserialize<StockCandle>(jsonText);
+
+    //    var result = (decimal)stockCandle.c.FirstOrDefault();
+
+    //    return result;
+    //}
 }
 
