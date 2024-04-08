545a0367c2419d20b1ac62c3b17f6fa98b977769
Onur Baglan <147552919+OnurBaglan@users.noreply.github.com>
2024-04-08 10:36:26 +0200

    trying yahoo

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using Csv;
+
+await DownloadCsv();
+
+Console.ReadKey();
+
+
+
+
+//var value = ReadStockValue("AMZN", new DateTime(2022, 01, 16));
+
+//decimal ReadStockValue(string stockSymbol, DateTime date)
+//{
+//    DownloadCsv(stockSymbol,date);
+
+//    var value= ReturnValueInFile(stockSymbol,date);
+
+//    return value;
+//}
+
+async Task DownloadCsv(/*string stockSymbol, DateTime date*/)
+{
+    var client = new HttpClient();
+
+    var response = await client.GetStringAsync("https://query1.finance.yahoo.com/v7/finance/download/AMZN?period1=1712102400&period2=1712534400&interval=1d&events=history&includeAdjustedClose=true");
+
+    var lines = response.Split("\n").ToList();
+
+    lines.RemoveAt(0);
+
+    var valuePairs = new Dictionary<string, string>();
+
+    foreach (var line in lines)
+    {
+        var cells = line.Split(",");
+
+        valuePairs.Add(cells[0], cells[4]); 
+    }
+
+    var result = valuePairs.OrderByDescending(pair => pair.Key).ToList().First().Value;
+
+
+}
